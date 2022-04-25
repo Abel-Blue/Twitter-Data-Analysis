0f6d3d891f3c267d2a8b18692a81dab4d74f6aa5
Abel Mitiku <abelmgetnet@gmail.com>
2022-04-25 22:56:39 +0300

    modify favorite count clearner

--- fix_bug/clean_tweets_dataframe.py
+++ fix_bug/clean_tweets_dataframe.py
@@ -2,14 +2,6 @@ import pandas as pd
 
 
 class CleanTweets:
-    """
-    This class is responsible for cleaning the twitter dataframe
-
-    Returns:
-    --------
-    A dataframe
-    """
-
     def __init__(self, df: pd.DataFrame):
         self.df = df
         print('Automation in Action...!!!')
