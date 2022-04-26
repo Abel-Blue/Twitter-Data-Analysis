96472b982da0131abdd0afb68c86543e78bb1d47
Abel Mitiku <abelmgetnet@gmail.com>
2022-04-26 14:53:22 +0300

    modified cleangit status

--- tests/test_clean_dataframe.py
+++ tests/test_clean_dataframe.py
@@ -1,3 +1,4 @@
+from clean_tweets_dataframe import Clean_Tweets
 import os
 import sys
 import unittest
@@ -6,12 +7,12 @@ import pandas.api.types as ptypes
 
 sys.path.append(os.path.abspath(os.path.join('fix_bug/')))
 
-from clean_tweets_dataframe import Clean_Tweets
 
 df = pd.read_csv("data/tweet_data.csv")
 
+
 class TestCleanTweetsDataframe(unittest.TestCase):
-    
+
     def setUp(self):
         self.df = df.copy(deep=True)
         self.cleaner = Clean_Tweets(self.df)
@@ -19,7 +20,7 @@ class TestCleanTweetsDataframe(unittest.TestCase):
     def test_drop_duplicate(self):
         df = self.cleaner.drop_duplicate(self.df)
         self.assertTrue(not df.duplicated().any())
-   
+
     def test_convert_to_datetime(self):
         df = self.cleaner.convert_to_datetime(self.df)
         self.assertTrue(type(df['created_at'].dtype == ptypes.DatetimeTZDtype))
@@ -34,16 +35,15 @@ class TestCleanTweetsDataframe(unittest.TestCase):
             ptypes.is_numeric_dtype(df['retweet_count']))
         self.assertTrue(
             ptypes.is_numeric_dtype(df['favorite_count']))
-        
-    
+
     def test_handle_missing_values(self):
-       df = self.cleaner.handle_missing_values(self.df)
-       self.assertTrue(df.isna().sum().sum() == 0)
-    
+        df = self.cleaner.handle_missing_values(self.df)
+        self.assertTrue(df.isna().sum().sum() == 0)
+
     def test_remove_non_english_tweets(self):
-      df = self.cleaner.remove_non_english_tweets(self.df)
-      df = df[df['lang'] != 'en']
-      self.assertEqual(df.shape[0], 0)
+        df = self.cleaner.remove_non_english_tweets(self.df)
+        df = df[df['lang'] != 'en']
+        self.assertEqual(df.shape[0], 0)
 
 
 if __name__ == '__main__':
